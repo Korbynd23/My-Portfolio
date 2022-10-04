85c212e880e67869554db7e9e8deeebd8586054f
kdicari24@gmail.com <kdicari24@gmail.com>
2022-10-04 09:53:40 -0400

    added forms to contact and media queries

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,13 +1,40 @@
 import React from 'react';
 
+// const styles = {
+//   textBox: {
+//     height: '100px',
+//     backgroundColor: 'white',
+//     width: '33%',
+//   }
+// }
+
 export default function Contact() {
   return (
     <div>
-      <h1>Contact Page</h1>
-      <p><a id="one" class="space" href="https://www.linkedin.com/in/korbyn-dicari-7a87a9244/" target="blank">LinkedIn</a></p>
-      <p><a id="two" class="space" href="https://github.com/Korbynd23" target="blank"> GitHub </a></p>  
-      <p> <a id="three" href="tel"> c:513-328-6704 </a></p> 
-      <p><a id="four" href="Kdicari24@gmai.com">Kdicari24@gmail.com </a></p>        
+      <h1>Contact Me</h1>
+      <form className='contact'>
+        <div className="row mb-3">
+          <label for="inputName" className="col-sm-2 col-form-label fs-5 fw-bold">Name:</label>
+        </div>
+        <div className="col-sm-4">
+          <input type="text" className="form-control" id="inputName"></input>
+        </div>
+        <div className="row mb-3">
+          <label for="inputEmail" className="col-sm-2 col-form-label fs-5 fw-bold">Email:</label>
+        </div>
+        <div className="col-sm-4">
+          <input type="email" className="form-control" id="inputEmail"></input>
+        </div>
+        <div className="row mb-3">
+          <label for="inputMessage" className="col-sm-2 col-form-label fs-5 fw-bold">Message:</label>
+        </div>
+        <div className="col-sm-14">
+          <textarea placeholder="Leave a comment here" className='mediaContact textBox'></textarea>
+          <label for="floatingTextarea2"></label>
+        </div>
+      <br></br>
+      <button type="submit" className="col-sm-1 btn btn-warning">Submit</button>
+      </form>
     </div>
   );
 }
