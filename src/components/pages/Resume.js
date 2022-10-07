7f1040d320b5907c562f54249715107db377ea85
kdicari24@gmail.com <kdicari24@gmail.com>
2022-10-06 21:14:55 -0400

    added css changes to portfolio

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -3,30 +3,36 @@ import React from 'react';
 export default function Resume() {
   return (
     <div>
-      <h2 className='resumeText'>Resume</h2>
+      <h2 className='resumeText'>_</h2>
       <div>
         {/* eslint-disable-next-line */}
         <a href="https://drive.google.com/file/d/1byUB9C7OGES8jeWgEHDVBhIsotEjnB8U/view?usp=sharing" target="_blank" className='resumeText'>Download Resume</a>
       </div>
-      <div className='skills'>
-        <br></br>
-        <h4>Front_end Proficiencies</h4>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>JavaScript</li>
-        <li>JQuery</li>
-        <li>responsive design</li>
-        <li>React</li>
-        <li>Bootstrap</li>
-        <br></br>
-        <h4>Back-end Proficiencies</h4>
-        <li>APIs</li>
-        <li>Node</li>
-        <li>Express</li>
-        <li>MySql Sequelize</li>
-        <li>MongoDB, Mongoose</li>
-        <li>REST</li>
-        <li>GraphQL</li>
+      <div className='skills' >
+        <div className='col-4 centered'>
+          <h4 className='resText'>Front-end Proficiencies:</h4>
+          <div className='pTag'>
+            <p>HTML</p>
+            <p>CSS</p>
+            <p>JavaScript</p>
+            <p>JQuery</p>
+            <p>responsive design</p>
+            <p>React</p>
+            <p>Bootstrap</p>
+          </div>
+        </div>
+        <div className='col-4 centered'>
+          <h4 className='resText'>Back-end Proficiencies:</h4>
+          <div className='pTag'>
+            <p>APIs</p>
+            <p>Node</p>
+            <p>Express</p>
+            <p>MySql Sequepze</p>
+            <p>MongoDB, Mongoose</p>
+            <p>REST</p>
+            <p>GraphQL</p>
+          </div>
+        </div>
       </div>
     </div>
   );
