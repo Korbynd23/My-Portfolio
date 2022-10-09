7dc4208e4a988634958f62651c0502f2b4ce1094
kdicari24@gmail.com <kdicari24@gmail.com>
2022-10-09 16:10:00 -0400

    saving changes to about me

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -7,8 +7,8 @@ export default function About() {
     <div className='aboutMe'>
       <div className='col-md-12 row justify-content-center background'>
         <p className='col-md-6 order-2 aboutText'>
-          Hello! My name is Korbyn Dicari, I am a Full-Stack bootcamp student at The Ohio State University (Grad date - 10/17/22). My skillset includes PWA's, Full-Stack web development, MySQL Databases, NoSQL databases, and React. I am always prepared to learn new skills and love a good challenge. In order to improve my skills, I do a lot of projects to keep my skills up to date.
-          On the home page, you can find my current work. I am always coding so check in once in a while, my home page may include some new work! Click on the contact
+          Hello! My name is Korbyn Dicari, I am a Full-Stack bootcamp student at The Ohio State University (Grad date - 10/17/22). My skillset includes PWA's, Full-Stack web development, MySQL Databases, NoSQL databases, and React. I am always prepared to learn new languages and love a good challenge. In order to stay up to date on my practices I enjoy continuously working on projects.
+          On the portfolio page, you can find my current work. I am always coding so check in once in a while, my home page may include some new work! Click on the contact
           tab if you want to get in touch with me, whether it's for collaboration on a project, a job offer, or even any questions! I welcome and invite you to get in
           contact with me.
         </p>
